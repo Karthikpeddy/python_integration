1dbbcdcc203183e2c5915533bec83c7d57ce8886
karthik peddy <117649571+Karthikpeddy@users.noreply.github.com>
2024-08-18 18:56:54 +0530

    Add files via upload
    commit 1

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'custom-dark-green': '#1c312c',
+      },
+    },
+  },
+  plugins: [],
+}
